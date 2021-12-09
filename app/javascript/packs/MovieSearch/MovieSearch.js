b6476d9e5e75ac0cbd9042915a6749bc45341af7
Fredrik Granvik <fredrik.granvik@tieto.com>
2021-12-09 08:45:15 +0100

    Added MovieSearch component

---
+++ app/javascript/packs/MovieSearch/MovieSearch.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieSearch = () => {
+  return <div>Movie Search</div>;
+};
+
+export default MovieSearch;
---
+++ app/javascript/packs/movie_search.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MovieSearch from "./MovieSearch/MovieSearch";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <MovieSearch />,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
