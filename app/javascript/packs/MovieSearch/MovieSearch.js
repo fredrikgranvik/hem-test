0950c23242b522043e0c590afcc25aabdd286053
Fredrik Granvik <fredrik.granvik@tieto.com>
2021-12-09 10:14:44 +0100

    added fetch for search query added react context to store response

--- app/javascript/packs/MovieSearch/MovieSearch.js
+++ app/javascript/packs/MovieSearch/MovieSearch.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import AppContext from "./AppContext";
 import SearchForm from "./SearchForm";
 import SearchResult from "./SearchResult";
 
 const MovieSearch = () => {
+  const [result, setResult] = useState();
+
   return (
-    <article>
-      <SearchForm />
-      <SearchResult />
-    </article>
+    <AppContext.Provider value={{ result, setResult }}>
+      <article>
+        <SearchForm />
+        <SearchResult />
+      </article>
+    </AppContext.Provider>
   );
 };
 
--- app/javascript/packs/MovieSearch/SearchForm.js
+++ app/javascript/packs/MovieSearch/SearchForm.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import AppContext from "./AppContext";
 
 const SearchForm = () => {
-  return <div>searchform</div>;
+  const context = useContext(AppContext);
+
+  const [inputValue, setInputValue] = useState("");
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const query = inputValue;
+
+    fetch("/search/" + query)
+      .then((res) => res.json())
+      .then((data) => {
+        context.setResult(data);
+      });
+  };
+
+  return (
+    <form onSubmit={(e) => submitForm(e)}>
+      <input
+        type="search"
+        value={inputValue}
+        name="searchQuery"
+        placeholder="Search for movie or series... fe. Title, Year.."
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button>Submit</button>
+    </form>
+  );
 };
 
 export default SearchForm;
